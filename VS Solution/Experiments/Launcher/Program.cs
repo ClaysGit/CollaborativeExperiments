7be19729b213ecd710df0ec100b8c5281d1a5fcb
Clayton Willey <clayton@careevolution.com>
2014-06-04 14:27:48 -0400

    Sample solution, illustrating simple separation of concepts

---
+++ VS Solution/Experiments/Launcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This references the other project, with the "action" code.
+// For this to work, you must first Right Click on the project,
+// select "Add Reference...", and then select the desired project in the Solution tab.
+// Having done that, all the code in that project can be called from this one. This line
+// just tells the compiler that we're using code from that Reference.
+using Library;
+
+namespace Launcher
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			//Get the first thing out of the input
+			var firstInput = "";
+			if ( args.Length > 0 )
+			{
+				firstInput = args[ 0 ];
+			}
+
+			//Create our ThingDoer
+			var thingDoer = new ThingDoer();
+
+			//Initialize it
+			thingDoer.Initialize();
+
+			//Use the ThingDoer to accomplish whatever the input asks of us
+			if ( String.Equals( firstInput, "action1" ) )
+			{
+				thingDoer.PerformSomeAction1();
+			}
+			else if ( String.Equals( firstInput, "action2" ) )
+			{
+				thingDoer.PerformSomeAction2();
+			}
+
+			//Let the ThinngDoer cleanup any resources it might be using.
+			thingDoer.Shutdown();
+		}
+	}
+}
---
+++ VS Solution/Experiments/Library/ThingDoer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; //I added this one because it has code for messing with Files (ie File.WriteAllText)
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+	public class ThingDoer
+	{
+		public void Initialize()
+		{
+			_message = "Hello World";
+		}
+
+		public void PerformSomeAction1()
+		{
+			Console.WriteLine( _message );
+		}
+
+		public void PerformSomeAction2()
+		{
+			File.WriteAllText( "PerformSomeAction2.txt", _message );
+		}
+
+		public void Shutdown()
+		{
+			//Normally this should be used to clean up any resources you're holding on to...
+			// But in this case we use it to make debugging a little nicer. Debugging a Command Line Program
+			// typically closes the Command Line window once it runs out of code. This gives us a pause point at the end.
+
+			//Pause until the Enter key is entered into the command line. 
+			Console.ReadLine();
+		}
+
+		private string _message = "";
+	}
+}
